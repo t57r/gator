394707f95ec5d4851269f31098c19a0cfe44d16b
t57r <parshenkotaras@gmail.com>
2025-03-09 23:21:06 +0200

    Config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/t57r/gator
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/t57r/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("t57r")
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+
+}
